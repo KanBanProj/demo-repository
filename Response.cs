6e9c174394cf06aad4ca4cfda27345d575fc035c
MASARWE <128424326+MASARWE@users.noreply.github.com>
2023-04-19 01:41:42 +0300

    Add files via upload

---
+++ Response.cs
@@ -0,0 +1,11 @@
+﻿
+    public class Response
+    {
+        public readonly string ErrorMessage;
+        public bool ErrorOccured { get => ErrorMessage != null; }
+        internal Response() { }
+        internal Response(string msg)
+        {
+            this.ErrorMessage = msg;
+        }
+    }
