0dcc046ce220666af67ac45d69862b138a0a2419
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2023-04-19 20:33:14 +0300

    Task step 1

---
+++ KanBan/Backend/ServiceLayer/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace IntroSE.KanBan.Backend.ServiceLayer
+{
+	public class Task
+	{
+        private int currColumn;
+        private int id;
+        private string Tittle;
+        private string Descreption;
+        private string dueDate;
+        private string CreationTime;
+        private int loc;
+
+        public Response ChangeTittle(string Tittle)
+        {
+            return new Response("not emplemented yet");
+        }
+    }
+}
