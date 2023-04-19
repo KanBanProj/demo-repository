b4acd183d8b7b1c3308332b690c0fc39f4fe86a9
bhaah <131205373+bhaah@users.noreply.github.com>
2023-04-19 23:54:27 +0300

    f_file2

---
+++ Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace KanBanProj.ServiceLayer
+{
+    internal class Task
+    {
+        private int currColumn;
+        private int id;
+        private string Tittle;
+        private string Descreption;
+        private string dueDate;
+        private string CreationTime;
+        private int loc;
+
+        public string ChangeTittle(string Tittle)
+        {
+            return JsonSerializer.Serialize(new Response("not emplemented yet"));
+        }
+
+        public string ChangeDescreptioon(string Descreption)
+        {
+
+            return JsonSerializer.Serialize(new Response("not emplemented yet"));
+
+        }
+        public string ChangeCol()
+        {
+            return JsonSerializer.Serialize(new Response("not emplemented yet"));
+        }
+
+        public string ChangeDueDate(string Date)
+        {
+            return JsonSerializer.Serialize(new Response("not emplemented yet"));
+        }
+    }
+
+}
+
